e385cca116460784faccf6b712a423b9d19ab069
robbie-cab <robbie.sinclair@cabinetoffice.gov.uk>
2022-03-24 17:07:25 +0000

    add gas chart structure

---
+++ charts/gas.py
@@ -0,0 +1,35 @@
+import geopandas
+from data.econ import gas_price
+
+def gas_data():
+    df = gas_price()
+    print(df)
+
+def gas_map(df, metric, fname):
+    gdf = gpd.read_file(RESOURCE_DIR + "worldmap.json")
+
+    gdf = pd.merge(gdf, df, left_on="iso_a3", right_on="iso3", how="inner")
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+    gdf.plot(
+        column=metric,
+        cmap="Reds",
+        linewidth=1,
+        ax=ax,
+        edgecolor="0.9",
+        # legend = False,
+        aspect="equal",
+    )
+    ax.axis("off")
+
+    lat = 48
+    lon = 31
+    lat_window = 30
+    lon_window = 50
+    ax.set_xlim(lon - lon_window, lon + lon_window)
+    ax.set_ylim(lat - lat_window, lat + lat_window)
+
+    fig.savefig(FIG_DIR + fname)
+
+
+
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from charts.corn import corn_hbar, corn_map, get_corn_data
+from charts.gas import gas_data
 
 
 def get_corn_charts():
@@ -9,6 +10,10 @@ def get_corn_charts():
     corn_hbar(df, "export %", "corn_exports_hbar.pdf")
     corn_hbar(df, "Imports per capita (kg)", "corn_imports_hbar.pdf")
 
+def get_gas_charts():
+    gas_data()
+
 
 if __name__ == "__main__":
-    get_corn_charts()
+    #get_corn_charts()
+    get_gas_charts()
