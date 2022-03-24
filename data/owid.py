5b0aa94ab333ddd5cee8c155d372f2f09e063551
robbie-cab <robbie.sinclair@cabinetoffice.gov.uk>
2022-03-24 11:58:12 +0000

    black flake8

--- data/owid.py
+++ data/owid.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 RESOURCE_DIR = "data/resources/"
 
-# https://ourworldindata.org/explorers/global-food?tab=chart&time=1998..latest&facet=none&Food=Maize+%28Corn%29&Metric=Exports&Per+Capita=false&country=USA~DEU~FRA~GBR~BRA~ZAF
+
 def corn_exports():
+    # https://ourworldindata.org/explorers/global-food?tab=chart&time=1998..latest&facet=none&Food=Maize+%28Corn%29&Metric=Exports&Per+Capita=false&country=USA~DEU~FRA~GBR~BRA~ZAF
     df = pd.read_csv(RESOURCE_DIR + "global-food.csv")
     metric = "Exports (t)"
     corn = df["Product"] == "Maize"
@@ -13,8 +14,8 @@ def corn_exports():
     return df.sort_values("Exports (t)")
 
 
-# https://aegis.acer.europa.eu/chest/dataitems/214/view
 def gas_imports():
+    # https://aegis.acer.europa.eu/chest/dataitems/214/view
     df = pd.DataFrame()
     data = pd.read_excel(
         RESOURCE_DIR + "Estimated number and diversity of supply sources  2020.xlsx",
@@ -28,7 +29,7 @@ def gas_imports():
         cdf = cdf.set_index("Year")
         for i, col in enumerate(source_cols):
             if cdf.loc[latest, col] == "RU":
-                df.loc[country, 'russian_gas'] = cdf.loc[latest, perc_cols[i]]
-                df.loc[country, 'year'] = latest
-    df = df.sort_values('russian_gas')
+                df.loc[country, "russian_gas"] = cdf.loc[latest, perc_cols[i]]
+                df.loc[country, "year"] = latest
+    df = df.sort_values("russian_gas")
     return df
