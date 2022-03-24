affa04a4ee287dd1673bcc1d0b26c9cbb3ca4911
robbie-cab <robbie.sinclair@cabinetoffice.gov.uk>
2022-03-24 16:48:49 +0000

    tidy

--- charts/corn.py
+++ charts/corn.py
@@ -1,11 +1,11 @@
-import pandas as pd
-import matplotlib
 import geopandas as gpd
+import matplotlib
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from data.iso import tidy_country_iso
 from data.owid import corn_exports, corn_imports
-from utils import RESOURCE_DIR, FIG_DIR
+from utils import FIG_DIR, RESOURCE_DIR
 
 
 def get_corn_data():
@@ -25,42 +25,43 @@ def get_corn_data():
 
 
 def corn_map(df, metric, fname):
-    gdf = gpd.read_file(RESOURCE_DIR + 'worldmap.json')
+    gdf = gpd.read_file(RESOURCE_DIR + "worldmap.json")
 
-    gdf = pd.merge(gdf, df, left_on='iso_a3', right_on='iso3', how='inner')
+    gdf = pd.merge(gdf, df, left_on="iso_a3", right_on="iso3", how="inner")
 
-    fig, ax = plt.subplots(figsize=(12,8))
+    fig, ax = plt.subplots(figsize=(12, 8))
     gdf.plot(
-            column=metric,
-            cmap='Reds',
-            linewidth=1, 
-            ax=ax, 
-            edgecolor='0.9', 
-            #legend = False,
-            aspect = 'equal',
+        column=metric,
+        cmap="Reds",
+        linewidth=1,
+        ax=ax,
+        edgecolor="0.9",
+        # legend = False,
+        aspect="equal",
     )
-    ax.axis('off')
+    ax.axis("off")
 
     lat = 48
     lon = 31
     lat_window = 30
     lon_window = 50
-    ax.set_xlim(lon-lon_window, lon+lon_window)
-    ax.set_ylim(lat-lat_window, lat+lat_window)
+    ax.set_xlim(lon - lon_window, lon + lon_window)
+    ax.set_ylim(lat - lat_window, lat + lat_window)
 
-    fig.savefig(FIG_DIR+fname)
+    fig.savefig(FIG_DIR + fname)
+
 
 def fig_axes(width=4, height=2.7):
     fig = matplotlib.figure.Figure(figsize=(width, height), frameon=True)
     ax = fig.add_subplot(111)
     fig.subplots_adjust(left=0.3, bottom=0, right=0.9, top=1)
 
-    spines = set(['bottom', 'top', 'left', 'right'])
+    spines = set(["bottom", "top", "left", "right"])
     show = set([])
     for spine in show:
         ax.spines[spine].set_edgecolor("lightgray")
         ax.spines[spine].set_linewidth(1)
-    for spine in (spines - show):
+    for spine in spines - show:
         ax.spines[spine].set_visible(False)
 
     ax.tick_params(axis="both", which="both", color="lightgray")
@@ -68,19 +69,17 @@ def fig_axes(width=4, height=2.7):
 
     return fig, ax
 
+
 def corn_hbar(df, metric, fname, n=7):
-    df = df.dropna(subset=['iso3', metric])
+    df = df.dropna(subset=["iso3", metric])
     df = df.sort_values(metric, ascending=True).tail(n)
 
     fig, ax = fig_axes(width=3.5, height=3)
-    ax.barh(df['Country'], df[metric])
+    ax.barh(df["Country"], df[metric])
 
     gap = df[metric].max() * 0.05
     for i in df.index:
         value = df.loc[i, metric]
-        ax.text(value+gap, df.loc[i, 'Country'], f"{value:.0f}%", ha="left", va="center")
+        ax.text(value + gap, df.loc[i, "Country"], f"{value:.0f}%", ha="left", va="center")
 
-
-    fig.savefig(FIG_DIR+fname)
-
-
+    fig.savefig(FIG_DIR + fname)
