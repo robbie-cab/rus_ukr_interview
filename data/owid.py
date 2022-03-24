1d4558014b4ea5f76fc9985335c880e08dc36118
robbie-cab <robbie.sinclair@cabinetoffice.gov.uk>
2022-03-24 15:56:12 +0000

    corn imports

--- data/owid.py
+++ data/owid.py
@@ -4,6 +4,15 @@ from data.iso import country_iso
 
 RESOURCE_DIR = "data/resources/"
 
+def corn_imports():
+    df = pd.read_csv(RESOURCE_DIR + "global-food.csv")
+    metrics = ['Imports per capita (kg)', 'Imports (t)']
+    corn = df["Product"] == "Maize"
+    df = df.loc[corn].dropna(subset=metrics)
+    latest = df["Year"] == df["Year"].max()
+    df = df.loc[latest, ["Country"] + metrics]
+    return df.sort_values(metrics[0])
+
 
 def corn_exports():
     # https://ourworldindata.org/explorers/global-food?tab=chart&time=1998..latest&facet=none&Food=Maize+%28Corn%29&Metric=Exports&Per+Capita=false&country=USA~DEU~FRA~GBR~BRA~ZAF
