6d268013fa204c5a857ba664ecf8d31913abdd21
robbie-cab <robbie.sinclair@cabinetoffice.gov.uk>
2022-03-24 16:48:31 +0000

    add corn hbar

--- charts/corn.py
+++ charts/corn.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
@@ -19,7 +20,7 @@ def get_corn_data():
     df = pd.merge(df, iso, how="outer")
 
     world = df.set_index("Country").loc["World", "Exports (t)"]
-    df["export %"] = df["Exports (t)"] / world
+    df["export %"] = 100 * df["Exports (t)"] / world
     return df
 
 
@@ -48,4 +49,38 @@ def corn_map(df, metric, fname):
     ax.set_ylim(lat-lat_window, lat+lat_window)
 
     fig.savefig(FIG_DIR+fname)
+
+def fig_axes(width=4, height=2.7):
+    fig = matplotlib.figure.Figure(figsize=(width, height), frameon=True)
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(left=0.3, bottom=0, right=0.9, top=1)
+
+    spines = set(['bottom', 'top', 'left', 'right'])
+    show = set([])
+    for spine in show:
+        ax.spines[spine].set_edgecolor("lightgray")
+        ax.spines[spine].set_linewidth(1)
+    for spine in (spines - show):
+        ax.spines[spine].set_visible(False)
+
+    ax.tick_params(axis="both", which="both", color="lightgray")
+    ax.grid(which="both", axis="both", visible=False)
+
+    return fig, ax
+
+def corn_hbar(df, metric, fname, n=7):
+    df = df.dropna(subset=['iso3', metric])
+    df = df.sort_values(metric, ascending=True).tail(n)
+
+    fig, ax = fig_axes(width=3.5, height=3)
+    ax.barh(df['Country'], df[metric])
+
+    gap = df[metric].max() * 0.05
+    for i in df.index:
+        value = df.loc[i, metric]
+        ax.text(value+gap, df.loc[i, 'Country'], f"{value:.0f}%", ha="left", va="center")
+
+
+    fig.savefig(FIG_DIR+fname)
+
 
