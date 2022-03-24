43557a1cf3e8ba3963d8ff65e54ab2a3e09f1bbc
robbie-cab <robbie.sinclair@cabinetoffice.gov.uk>
2022-03-24 16:12:11 +0000

    map of corn exports

--- charts/corn.py
+++ charts/corn.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
 from data.iso import tidy_country_iso
-from data.owid import corn_exports
+from data.owid import corn_exports, corn_imports
+from utils import RESOURCE_DIR, FIG_DIR
 
 
 def get_corn_data():
@@ -18,3 +21,29 @@ def get_corn_data():
     world = df.set_index("Country").loc["World", "Exports (t)"]
     df["export %"] = df["Exports (t)"] / world
     return df
+
+
+def corn_map(df, metric):
+    gdf = gpd.read_file(RESOURCE_DIR + 'worldmap.json')
+
+    gdf = pd.merge(gdf, df, left_on='iso_a3', right_on='iso3', how='inner')
+
+    fig, ax = plt.subplots(figsize=(12,8))
+    gdf.plot(
+            column=metric,
+            cmap='Reds',
+            linewidth=1, 
+            ax=ax, 
+            edgecolor='0.9', 
+            #legend = False,
+            aspect = 'equal',
+    )
+    ax.axis('off')
+
+    #lat = 48
+    #lon = 31
+    #ax.set_xlim(lon-15, lon+15)
+    #ax.set_ylim(lat-10, lat+10)
+
+    fig.savefig(FIG_DIR+'corn_map.pdf')
+
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import pathlib
 import diskcache
 
 RESOURCE_DIR = "data/resources/"
+FIG_DIR = "figures/"
 
 CACHE_TIME_LIMIT = 60 * 60 * 12
 __here__ = pathlib.Path(__file__).parent
